c155d956ace2b181ea2a488f9ecc4b165524a7af
Murphy ORourke <murphy.orourke@spr.com>
2020-01-28 23:16:25 -0600

    Initial commit, project bootstrap.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # flexbox
+
+Learning exercises and personal reference for CSS Flexbox.
+
+Started January 2020
---
+++ js/app.js
@@ -0,0 +1,15 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+// App function
+var app = function ($) {
+
+};
+
+// Run app function on ready
+$(document).ready(app);
+
+// Resize behavior
+$(window).resize(function() {
+
+});
